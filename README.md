d92556e8de04ca87663005a98bea61cc21125650
jburky15 <jburky15@gmail.com>
2023-06-22 15:40:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Old Portfolio Page
+
+I found this old portfolio page kicking around on my external hard drive. Figured I would upload it to github as a showcase piece at some point.
---
+++ scripts/work.js
@@ -0,0 +1,14 @@
+const hiddenSections = document.querySelectorAll('.hidden')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+hiddenSections.forEach((el) => observer.observe(el))
